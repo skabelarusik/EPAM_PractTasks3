7675e892c6d42b4ad296fc060f7bb6a17dda3995
skabelarusik <43265848+skabelarusik@users.noreply.github.com>
2019-02-24 12:56:25 +0300

    Update AriphGeomAverage.java

--- AriphGeomAverage.java
+++ AriphGeomAverage.java
@@ -11,48 +11,16 @@ public class AriphGeomAverage {
 
 class Helper {
 
-    public static double averAriph(int a){
-        if(!checkNumbers(a)){
-            return -1;
-        }
-
-        int sum = 0;
-        while (a>0){
-            sum += a%10;
-            a /= 10;
-        }
-
-        return (double)sum/6;
+    public static double averAriph(int a) {
+        return (double)(a % 10 + (a /= 10) % 10 + (a /= 10) % 10 + (a /= 10)
+                % 10 + (a /= 10) % 10 + (a/=10) % 10)/6;
     }
 
-    public static double averGeom(int a){
-        if(!checkNumbers(a)){
-            return -1;
-        }
 
-        int mult = 1;
+    public static double averGeom(int a) {
 
-        while (a > 0){
-            mult *= a%10;
-            a /= 10;
-        }
-        System.out.println(mult);
-
-        return Math.pow((double)mult, (double)1/6);
+        return Math.pow((double) (a % 10 * ((a /= 10) % 10) * ((a /= 10) % 10) *
+                ((a /= 10) % 10) * ((a /= 10) % 10) * ((a /= 10) % 10)), (double) 1 / 6);
     }
 
-    public static boolean checkNumbers(int a) {
-        int count = 0;
-        while (a > 0){
-            a/=10;
-            count++;
-        }
-
-        if(count != 6) {
-            System.out.println("ERROR");
-            return false;
-        }
-
-        return true;
-    }
 }
